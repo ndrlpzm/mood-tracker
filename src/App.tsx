16a3de739c3629443f986c25bddac41fe587caea
ndrlpzm <andrea.lm923@gmail.com>
2023-07-28 12:06:57 +0200

    adds the wireframe of a very basic mood

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MoodDetail from './detail/MoodDetail';
 
 function App() {
+  const [moods, setMoods] = useState([]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main>
+        <MoodDetail></MoodDetail>
+      </main>
     </div>
   );
 }
---
+++ src/detail/MoodDetail.tsx
@@ -0,0 +1,14 @@
+
+function MoodDetail(){
+	return(<article>
+		<div>
+			<button>1</button>
+			<button>2</button>
+			<button>3</button>
+			<button>4</button>
+			<button>5</button>
+		</div>
+		<button>Save</button>
+		</article>);
+}
+export default MoodDetail;

7d442b91ecd300c05acaee163a4252c787e27d24
ndrlpzm <andrea.lm923@gmail.com>
2023-08-02 13:03:37 +0200

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import React,{ useState } from 'react';
-import './App.css';
-import MoodDetail from './detail/MoodDetail';
-import { Mood } from './detail/mood';
+import React, { useState } from "react";
+import "./App.css";
+import MoodDetail from "./detail/MoodDetail";
+import { Mood } from "./detail/mood";
 
 function App() {
   const [moodList, setMoodList] = useState(new Array<Mood>());
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <header className="App-header"></header>
       <main>
         <MoodDetail moodList={moodList} setMoodList={setMoodList}></MoodDetail>
       </main>
--- src/detail/MoodDetail.tsx
+++ src/detail/MoodDetail.tsx
@@ -1,37 +1,43 @@
 import React, { useState } from "react";
 import { Mood } from "./mood";
-interface MoodInput{
-	moodList:Mood[],
-	setMoodList: React.Dispatch<React.SetStateAction<Mood[]>>
+interface MoodInput {
+  moodList: Mood[];
+  setMoodList: React.Dispatch<React.SetStateAction<Mood[]>>;
 }
-function MoodDetail({moodList, setMoodList}: MoodInput){
-	const [mood, setMood] = useState(new Mood());
-	const handleClickRating = (e: React.MouseEvent<HTMLButtonElement>) => {
-		e.preventDefault();
-		setMood({
-				...mood,
-				value: parseInt(e.currentTarget.value)
-				// Fix parse int, remove when value is actually a number
-			});
-	};
-	const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
-		e.preventDefault();
-		setMoodList([...moodList, mood]);
-		console.log(moodList);
-	};
-	const ratings: number[] = [];
-	for (let i = 1; i < 6; i++) {
-		ratings.push(i)
-	};
-	return(
-	<article>
-		<div>
-			{ratings.map(function (i) {
-				return <button onClick={handleClickRating} key={i} value={i}>{i}</button>;
-			})}
-		</div>
-		<button onClick={handleSubmit}>Save</button>
-		</article>
-		);
+function MoodDetail({ moodList, setMoodList }: MoodInput) {
+  const [mood, setMood] = useState(new Mood());
+  const handleClickRating = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setMood({
+      ...mood,
+      value: parseInt(e.currentTarget.value),
+      // Fix parse int, remove when value is actually a number
+    });
+  };
+  const handleSubmit = (e: React.MouseEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setMoodList([...moodList, mood]);
+    console.log(moodList);
+  };
+  const ratings: number[] = [];
+  for (let i = 1; i < 6; i++) {
+    ratings.push(i);
+  }
+  return (
+    <article>
+      <form onSubmit={handleSubmit}>
+        <div>
+          {ratings.map(function (i) {
+            return (
+              <button onClick={handleClickRating} key={i} value={i}>
+                {i}
+              </button>
+            );
+          })}
+        </div>
+        <input type="submit" value="Save" />
+      </form>
+    </article>
+  );
 }
 export default MoodDetail;

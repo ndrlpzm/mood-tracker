fa10055cee58e4c99f396b94482d5af2b61bdb79
ndrlpzm <andrea.lm923@gmail.com>
2023-08-06 12:02:11 +0200

    api mock skeleton

---
+++ src/data/apiMock.ts
@@ -0,0 +1,14 @@
+export function addMood(){
+	setTimeout(() => {}, 2000);
+}
+export function deleteMood(){
+	setTimeout(() => {}, 2000);
+}
+export function returnLatestMoods(){
+	//returns moods ordered by date-time
+	setTimeout(() => {}, 2000);
+}
+export function returnMoodsByDate(startingDate:Date, endDate:Date){
+	//returns moods within a date range
+	setTimeout(() => {}, 2000);
+}

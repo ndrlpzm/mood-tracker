87bb72c00dd9a328eff1f23cb2e51522a1624865
ndrlpzm <andrea.lm923@gmail.com>
2023-08-09 16:56:56 +0200

    mocks delayed retrieval of data

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import useSWR from "swr";
+import { returnLatestMoods } from "./data/apiMock";
 import "./App.css";
 import MoodDetail from "./detail/MoodDetail";
+import Home from "./home/Home";
 import { Mood } from "./detail/mood";
 
 function App() {
   const [moodList, setMoodList] = useState(new Array<Mood>());
+  const { data, error, isLoading } = useSWR(
+    "../data/apiMock.ts",
+    returnLatestMoods
+  );
+  useEffect(() => {
+    setMoodList(data ?? []);
+    console.log(data);
+  }, [data]);
   return (
     <div className="App">
       <header className="App-header"></header>
       <main>
+        <Home moodList={moodList} isLoading={isLoading}></Home>
         <MoodDetail moodList={moodList} setMoodList={setMoodList}></MoodDetail>
       </main>
     </div>

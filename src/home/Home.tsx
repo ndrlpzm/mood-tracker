06acb0267962339687ba28a8818cdb880b3259db
ndrlpzm <andrea.lm923@gmail.com>
2023-08-10 17:02:42 +0200

    groups data by date

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,13 +1,30 @@
-
 import ClipLoader from "react-spinners/ClipLoader";
 import { Mood } from "../detail/mood";
+import React from "react";
 
 interface HomeInput {
   moodList: Mood[];
   isLoading: boolean;
 }
-function Home({ moodList, isLoading}: HomeInput) {
-  
+function Home({ moodList, isLoading }: HomeInput) {
+  const formattedData = () => {
+    var prevDate: Date;
+    const formattedElements = moodList.map((mood) => {
+      const art: JSX.Element = (
+        <article key={mood.value}>
+          <div>{mood.value}</div>
+          <div>{mood.comment}</div>
+        </article>
+      );
+      if (prevDate !== mood.date) {
+        const header: JSX.Element = <h3>{mood.date.toLocaleDateString()}</h3>;
+        return [header, art];
+      } else {
+        return art;
+      }
+    });
+    return formattedElements;
+  };
   return (
     <>
       {isLoading ? (
@@ -17,7 +34,7 @@ function Home({ moodList, isLoading}: HomeInput) {
           data-testid="loader"
         />
       ) : (
-        <>{moodList.length}</>
+        <>{formattedData()}</>
       )}
     </>
   );

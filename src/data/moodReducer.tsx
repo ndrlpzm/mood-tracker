eea2a25ecbdc25f8452000e819a6ef50511756d7
ndrlpzm <andrea.lm923@gmail.com>
2023-08-15 13:04:28 +0200

    adds moods reducer

---
+++ src/data/moodReducer.tsx
@@ -0,0 +1,31 @@
+import { Mood } from "../detail/mood";
+export type MoodActionType="add" | "delete" | "replace";
+export class MoodAction{
+	constructor(
+		public type:MoodActionType,
+		public mood: Mood,
+		public moodIndex:number,
+		public newList:Mood[]
+	){}
+}
+export default function moodsReducer(moods:Mood[],action: MoodAction){
+	switch(action.type){
+		case "add":{
+			console.log("add");
+			return [...moods, action.mood];
+		}
+		case "delete":{
+			console.log("delete");
+			return moods.filter((value, idx) => action.moodIndex !== idx);
+		}
+		case "replace":{
+			console.log("replace");
+			return action.newList;
+		}
+		default:{
+			console.log("Unknown action type");
+			return moods;
+		}
+	}
+
+}

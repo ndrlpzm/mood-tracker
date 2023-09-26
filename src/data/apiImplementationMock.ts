3af0aec19e0d4cc8a9e991d1710e4ab42b9d8212
ndrlpzm <andrea.lm923@gmail.com>
2023-09-26 12:31:03 +0200

    moves api mocking to a new file

---
+++ src/data/apiImplementationMock.ts
@@ -0,0 +1,83 @@
+import { Mood } from "./classes/mood";
+import { Tag } from "./classes/tag";
+
+export async function retrieveMoodMock(url: string, id: number) {
+  const results = storedMoodList.find((x) => {
+    if (x.id === id) return x;
+  });
+  return results;
+}
+export async function addMoodMock(url: string, mood: Mood) {
+  var maxIdElement = storedMoodList.reduce((prevMood, curMood) => {
+    return (prevMood = prevMood.id > curMood.id ? prevMood : curMood);
+  });
+  const newMood = { ...mood, id: maxIdElement.id + 1 };
+  storedMoodList.push(newMood);
+  return newMood;
+}
+export async function updateMoodMock(url: string, mood: Mood) {
+  storedMoodList = storedMoodList.map((x) => {
+    if (x.id === mood.id) return mood;
+    else return x;
+  });
+  return storedMoodList;
+}
+export function deleteMoodMock(url: string, mood: Mood) {
+  const index = storedMoodList.indexOf(mood);
+  storedMoodList = storedMoodList.splice(index, 1);
+}
+export async function returnLatestMoodsMock() {
+  const orderDatesDesc = (prevMood: Mood, curMood: Mood) => {
+    return prevMood.date.valueOf() - curMood.date.valueOf();
+  };
+  const orderedMoodList = storedMoodList.map((x) => x);
+  orderedMoodList.sort(orderDatesDesc);
+  return orderedMoodList;
+}
+export function returnAvailableTagsMock(selectedTags: Tag[]) {
+  console.log("returnAvailableTags mock");
+  return storedTagList.filter((x) => !selectedTags.includes(x));
+}
+/*------------------------------------------------------------------- */
+const now = new Date();
+var storedMoodList: Mood[] = [
+  {
+    id: 1,
+    value: 4,
+    date: now,
+    comment: "Great weather",
+    tags: [{ id: 1, value: "Sad", color: "blue" }],
+  },
+  {
+    id: 2,
+    value: 2,
+    date: getDate(1),
+    comment: "Received bad news",
+    tags: [{ id: 2, value: "Angry", color: "red" }],
+  },
+  { id: 3, value: 3, date: getDate(2), comment: "", tags: [] },
+  { id: 4, value: 5, date: getDate(2), comment: "Went on a trip", tags: [] },
+  {
+    id: 5,
+    value: 1,
+    date: getDate(4),
+    comment: "Got hurt",
+    tags: [
+      { id: 4, value: "Frustrated", color: "orange" },
+      { id: 3, value: "Anxious", color: "green" },
+    ],
+  },
+];
+var storedTagList: Tag[] = [
+  { id: 1, value: "Sad", color: "blue" },
+  { id: 2, value: "Angry", color: "red" },
+  { id: 3, value: "Anxious", color: "green" },
+  { id: 4, value: "Frustrated", color: "orange" },
+  { id: 5, value: "Tired", color: "gray" },
+];
+
+function getDate(addedDays: number) {
+  const today = new Date();
+  today.setDate(now.getDate() + addedDays);
+  return today;
+}
--- src/data/apiMock.ts
+++ src/data/apiMock.ts
@@ -1,66 +1,37 @@
 import { Mood } from "./classes/mood";
 import { Tag } from "./classes/tag";
+import {addMoodMock, deleteMoodMock, retrieveMoodMock, returnAvailableTagsMock, returnLatestMoodsMock, updateMoodMock} from "./apiImplementationMock.ts";
 
 export async function retrieveMood(url: string, id: number) {
   console.log("retrieveMood mock");
   await delay(100);
-  const results= storedMoodList.find((x)=>{if(x.id===id) return x;});
-  return results;
+  return retrieveMoodMock(url,id);
 }
 export async function addMood(url: string, mood: Mood) {
   console.log("addMood mock");
   await delay(500);
-  var maxIdElement = storedMoodList.reduce((prevMood, curMood) => {
-    return (prevMood = prevMood.id > curMood.id ? prevMood : curMood);
-  });
-  const newMood = { ...mood, id: maxIdElement.id + 1 };
-  storedMoodList.push(newMood);
-  return newMood;
+  return addMoodMock(url,mood);
+}
+export async function updateMood(url: string, mood: Mood) {
+  console.log("updateMood mock");
+  await delay(100);
+  return updateMoodMock(url,mood);
 }
 export function deleteMood(url: string, mood: Mood) {
   console.log("deleteMood mock");
-  const index = storedMoodList.indexOf(mood);
-  storedMoodList = storedMoodList.splice(index, 1);
+  return deleteMoodMock(url,mood);
 }
 export async function returnLatestMoods() {
   console.log("returnLatestMoods mock");
   //returns moods ordered by date-time
   await delay(1500);
-  const orderDatesDesc = (prevMood: Mood, curMood: Mood) => {
-    return prevMood.date.valueOf() - curMood.date.valueOf();
-  };
-  const orderedMoodList = storedMoodList.map(x=>x);
-  orderedMoodList.sort(orderDatesDesc);
-  return orderedMoodList;
+  return returnLatestMoodsMock();
 }
-export function  returnAvailableTags(selectedTags:Tag[]){
+export function returnAvailableTags(selectedTags: Tag[]) {
   console.log("returnAvailableTags mock");
-  return storedTagList.filter(x => !selectedTags.includes(x));
+  return returnAvailableTagsMock(selectedTags);
 }
-/*------------------------------------------------------------------- */
-const now = new Date();
-var storedMoodList: Mood[] = [
-  { id: 1, value: 4, date: now, comment: "Great weather", tags:[{ id: 1, value: "Sad", color: "blue" }] },
-  { id: 2, value: 2, date: getDate(1), comment: "Received bad news", tags:[{ id: 2, value: "Angry", color: "red"  }]  },
-  { id: 3, value: 3, date: getDate(2), comment: "", tags:[]  },
-  { id: 4, value: 5, date: getDate(2), comment: "Went on a trip", tags:[]  },
-  { id: 5, value: 1, date: getDate(4), comment: "Got hurt", tags:[{ id: 4, value: "Frustrated", color: "orange"  },
-  { id: 3, value: "Anxious", color: "green"  }]  },
-];
-var storedTagList: Tag[] = [
-  { id: 1, value: "Sad", color: "blue" },
-  { id: 2, value: "Angry", color: "red"  },
-  { id: 3, value: "Anxious", color: "green"  },
-  { id: 4, value: "Frustrated", color: "orange"  },
-  { id: 5, value: "Tired", color: "gray"  },
-];
 
 function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
-function getDate(addedDays: number) {
-  const today = new Date();
-  today.setDate(now.getDate() + addedDays);
-  return today;
-}
-

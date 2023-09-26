2aa3ef990f0bed86b3c3d7bb3dd962840071c09f
ndrlpzm <andrea.lm923@gmail.com>
2023-09-26 17:23:24 +0200

    formatting

--- src/data/apiMock.ts
+++ src/data/apiMock.ts
@@ -1,34 +1,37 @@
 import { Mood } from "./classes/mood";
 import { Tag } from "./classes/tag";
-import {addMoodMock, deleteMoodMock, retrieveMoodMock, returnAvailableTagsMock, returnLatestMoodsMock, updateMoodMock} from "./apiImplementationMock.ts";
+import {
+  addMoodMock,
+  deleteMoodMock,
+  retrieveMoodMock,
+  returnAvailableTagsMock,
+  returnLatestMoodsMock,
+  updateMoodMock,
+} from "./apiImplementationMock.ts";
 
 export async function retrieveMood(url: string, id: number) {
-  console.log("retrieveMood mock");
   await delay(100);
-  return retrieveMoodMock(url,id);
+  return retrieveMoodMock(url, id);
 }
 export async function addMood(url: string, mood: Mood) {
-  console.log("addMood mock");
   await delay(500);
-  return addMoodMock(url,mood);
+  return addMoodMock(url, mood);
 }
 export async function updateMood(url: string, mood: Mood) {
-  console.log("updateMood mock");
   await delay(100);
-  return updateMoodMock(url,mood);
+  return updateMoodMock(url, mood);
 }
 export function deleteMood(url: string, mood: Mood) {
   console.log("deleteMood mock");
-  return deleteMoodMock(url,mood);
+  return deleteMoodMock(url, mood);
 }
+
 export async function returnLatestMoods() {
-  console.log("returnLatestMoods mock");
   //returns moods ordered by date-time
   await delay(1500);
   return returnLatestMoodsMock();
 }
 export function returnAvailableTags(selectedTags: Tag[]) {
-  console.log("returnAvailableTags mock");
   return returnAvailableTagsMock(selectedTags);
 }
 

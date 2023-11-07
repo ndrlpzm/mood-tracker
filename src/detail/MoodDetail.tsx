134c37f08133779b90cb533c8029a165cb691c9f
ndrlpzm <andrea.lm923@gmail.com>
2023-11-07 13:42:58 +0100

    Fixes back button

--- src/detail/MoodDetail.tsx
+++ src/detail/MoodDetail.tsx
@@ -1,13 +1,16 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import useSWR from "swr";
 import { retrieveMood } from "../data/apiMock";
 import { MoodDetailForm } from "./MoodDetailForm";
 import "./MoodDetail.css";
 import DeleteButton from "./DeleteButton";
+import IconButton from "../common-components/IconButton";
+import leftArrowIcon from "../resources/icons8-chevron-left-50.png";
 
 export const MoodDetail = () => {
   const { moodId } = useParams();
   const parsedMoodId = parseInt(moodId ?? "");
+  const navigate = useNavigate();
 
   const { data: mood, isLoading } = useSWR("/mood", (url) =>
     retrieveMood(url, parsedMoodId)
@@ -21,11 +24,11 @@ export const MoodDetail = () => {
     <div className="mood-detail">
       <nav>
         <div className="nav-left">
-          {/* <IconButton
-	  displayImg={leftArrowIcon}
-	  callbackFunction={goBack}
-	  description="Back"
-	></IconButton> */}
+          <IconButton
+            displayImg={leftArrowIcon}
+            callbackFunction={() => navigate(-1)}
+            description="Back"
+          ></IconButton>
         </div>
         <div className="nav-right">
           <DeleteButton id={mood?.id}></DeleteButton>
